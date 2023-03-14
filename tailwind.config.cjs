5d7e5bc6e4e026aadd16cd460f0c1a3bb49d97b2
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-15 00:30:52 +0300

    feat: add daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,13 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+
+    //...
+    plugins: [require("daisyui")],
+    daisyui: {
+      themes: ["winter", "dark", "light"],
+    },
+
 };
 
 module.exports = config;
