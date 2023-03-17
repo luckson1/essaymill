899b9ff9936d446c7f1f201ab6bb5d85f3342b7e
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-18 01:55:49 +0300

    fix:layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,11 @@ const subjects= await prisma.subject.findMany()
   return (
     <>
    
-      <main className="flex h-fit min-h-[calc(100vh-16px)] w-full flex-col items-center justify-center  " >
-    <Interactivity subjects={subjects}/>
+      <main className="flex h-fit min-h-[calc(100vh-16px)]  bg-gradient-to-b from-[#2e026d] to-[#15162c] w-full flex-col items-center justify-center  " >
+  <div className="w-full md:w-[90%] lg:w-[80%]  px-5 md:px-0">
+
+  <Interactivity subjects={subjects}/>
+  </div>
       
       </main>
     </>
