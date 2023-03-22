f680d50bd30ec1bc0e8a2f9de86c641d0e6ea4dd
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-22 15:13:44 +0300

    fix: views based on roles

---
+++ src/pages/drafts/index.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from '@mui/material'
+import { useSession } from 'next-auth/react'
+import React from 'react'
+import Admin from '~/components/adminPages/Admin'
+import Customers from '~/components/customerPage/Customer'
+
+const Index = () => {
+  const session=useSession()
+  const status=session.status
+  const isLoading=status==="loading"
+  const role=session.data?.user.role
+  const admin=role==="admin"
+  const customer=role==="customer"
+  if(isLoading)return( <div className='w-full h-[40vh]'><Skeleton/></div>)
+  return (
+<>
+{admin? <Admin status='draft' /> : customer? <Customers status='draft' /> : <div className='my-32 mx-32 text-red-500'>Anuthorised</div>}
+</>
+  )
+}
+
+export default Index
