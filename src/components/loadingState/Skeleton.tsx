d9e576e07ff611759dd77bed1d077fbe6f698967
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-03-24 17:31:44 +0300

    style: remove too much shadow

--- src/components/loadingState/Skeleton.tsx
+++ src/components/loadingState/Skeleton.tsx
@@ -6,12 +6,12 @@ const Skeleton = ()  => {
       <div className="flex w-full h-full min-h-fit flex-1 flex-col items-center  px-20">
         <div className="mt-12 w-full min-h-fit  h-full animate-pulse flex-row items-center justify-center space-x-1 rounded-xl border p-6 ">
           <div className="flex flex-col space-y-4 h-full min-h-fit ">
-            <div className="min-h-6 min-w-6   h-[15%] w-12/12 rounded-md bg-slate-100 shadow-md shadow-gray-500/100"></div>
-            <div className="min-h-6 min-w-6 h-[15%] w-11/12 rounded-md bg-slate-100 shadow-md  shadow-gray-500/100"></div>
-            <div className="min-h-6 min-w-6 h-[15%] w-10/12 rounded-md bg-slate-100 shadow-md shadow-gray-500/100"></div>
-            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow-md shadow-gray-500/100"></div>
-            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow-md shadow-gray-500/100"></div>
-            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow-md shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6   h-[15%] w-12/12 rounded-md bg-slate-100 shadow shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6 h-[15%] w-11/12 rounded-md bg-slate-100 shadow  shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6 h-[15%] w-10/12 rounded-md bg-slate-100 shadow shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow shadow-gray-500/100"></div>
+            <div className="min-h-6 min-w-6 h-[15%] w-9/12 rounded-md bg-slate-100 shadow shadow-gray-500/100"></div>
           </div>
         </div>
       </div>
