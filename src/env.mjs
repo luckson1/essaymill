1e8d6a56750893e802e57ee7a5d348d1d5d5a6a1
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-11 17:43:53 +0300

    fix: set smtp server

--- src/env.mjs
+++ src/env.mjs
@@ -16,7 +16,7 @@ const server = z.object({
     // Since NextAuth.js automatically uses the VERCEL_URL if present.
     (str) => process.env.VERCEL_URL ?? str,
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string().min(1) : z.string().url(),
+    process.env.VERCEL ? z.string().min(1) : z.string().url()
   ),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   CLIENT_ID: z.string(),
@@ -26,7 +26,12 @@ const server = z.object({
   BUCKET_NAME: z.string(),
   REGION: z.string(),
   EMAIL_SERVER: z.string(),
-EMAIL_FROM: z.string(),
+  EMAIL_FROM: z.string(),
+  EMAIL_SERVER_HOST: z.string(),
+  EMAIL_SERVER_PORT: z.string(),
+
+  EMAIL_SERVER_USER:z.string(),
+  EMAIL_SERVER_PASSWORD: z.string(),
 });
 
 /**
@@ -51,12 +56,17 @@ const processEnv = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
   ACCESS_KEY: process.env.ACCESS_KEY,
-  SECRET_KEY: process.env. SECRET_KEY,
+  SECRET_KEY: process.env.SECRET_KEY,
   BUCKET_NAME: process.env.BUCKET_NAME,
   REGION: process.env.REGION,
   EMAIL_SERVER: process.env.EMAIL_SERVER,
-  EMAIL_FROM:process.env.EMAIL_FROM,
-  
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+  EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+
+  EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+  EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
@@ -83,7 +93,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
   if (parsed.success === false) {
     console.error(
       "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
+      parsed.error.flatten().fieldErrors
     );
     throw new Error("Invalid environment variables");
   }
@@ -97,7 +107,7 @@ if (!!process.env.SKIP_ENV_VALIDATION == false) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
